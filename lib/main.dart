7505240c8462d4a3304bcb9e4e8a0d87c65f1865
alijumaan <alila3883@gmail.com>
2021-10-25 22:08:02 +0300

    create login form

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter app'),
+          title: const Text('Login'),
         ),
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 28),
-          ),
+        body: Column(
+          children: <Widget> [
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: 'Email'
+              ),
+            ),
+            const TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                  labelText: 'Password'
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () => print('login clicked'),
+                child: const Text('Login'),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 36)),
+            )
+          ],
         ),
       )
     );
